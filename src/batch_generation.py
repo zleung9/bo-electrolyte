0a53b0bbe39b1c3176057f25b19950c3bd417037
zleung9 <zhul9311@gmail.com>
2023-03-17 13:18:23 -0700

    TURBO [V1.0]: First draft of TURBO algorithm in the new API framework

--- src/batch_generation.py
+++ src/batch_generation.py
@@ -22,6 +22,47 @@ from gpytorch.priors import HorseshoePrior
 import numpy as np
 
 
+@dataclass
+class TurboState:
+    dim: int
+    batch_size: int
+    length: float = 0.8
+    length_min: float = 0.5 ** 7
+    length_max: float = 1.6
+    failure_counter: int = 0
+    failure_tolerance: int = float("nan")  # Note: Post-initialized
+    success_counter: int = 0
+    success_tolerance: int = 10  # Note: The original paper uses 3
+    best_value: float = -float("inf")
+    restart_triggered: bool = False
+
+    def __post_init__(self):
+        self.failure_tolerance = math.ceil(
+            max([4.0 / self.batch_size, float(self.dim) / self.batch_size])
+        )
+
+
+def update_state(state, Y_next):
+    if max(Y_next) > state.best_value + 1e-3 * math.fabs(state.best_value):
+        state.success_counter += 1
+        state.failure_counter = 0
+    else:
+        state.success_counter = 0
+        state.failure_counter += 1
+
+    if state.success_counter == state.success_tolerance:  # Expand trust region
+        state.length = min(2.0 * state.length, state.length_max)
+        state.success_counter = 0
+    elif state.failure_counter == state.failure_tolerance:  # Shrink trust region
+        state.length /= 2.0
+        state.failure_counter = 0
+
+    state.best_value = max(state.best_value, max(Y_next).item())
+    if state.length < state.length_min:
+        state.restart_triggered = True
+    return state
+
+
 def generate_batch(state, model, X, Y,  raw_samples: int,
                    batch_size, device, n_candidates=None, num_restarts=10,
                    acqf="ts", dtype = torch.double):
@@ -92,3 +133,5 @@ def generate_batch(state, model, X, Y,  raw_samples: int,
         raise ValueError
 
     return X_next
+
+
