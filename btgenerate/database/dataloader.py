c745e8f162a7295e094b74c649604ede400a5880
zleung9 <zhul9311@gmail.com>
2023-08-21 21:31:14 -0700

    Add two method to `LiquidMasterTableDataset`: `reduced_space` and `parallel_plot`

--- btgenerate/database/dataloader.py
+++ btgenerate/database/dataloader.py
@@ -3,6 +3,7 @@ import os
 import torch
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 from btgenerate.database.database import Database
 
@@ -12,7 +13,7 @@ class AutomatDataSet(Dataset):
     def __init__(self, df:pd.DataFrame=None):
         super().__init__()
         self._df = df.copy()
-    
+
     @property
     def dataframe(self):
         return self._df.copy()
@@ -70,7 +71,6 @@ class LiquidMasterTableDataSet(AutomatDataSet):
         if df is None:
             df = Database(db=self.database).pull(table=self.table)
         super().__init__(df=df)
-        self.lce_name = "LCE"
         self.info_columns = [ # columns except chemicals
             'Electrolyte ID', 'lab_batch', 'note', 'total_mass(g)', 'generation_method', 
             'generation_project', 'experiment', 'Conductivity', 'Voltage', 'Cycles', 'LCE', 
@@ -78,12 +78,7 @@ class LiquidMasterTableDataSet(AutomatDataSet):
             'Predicted Voltage', 'Predicted LCE'
         ]
         self.chemical_names = df.columns[~df.columns.isin(self.info_columns)].tolist()
-        self._name_dict = {
-            "1,2-dimethoxyethane": "DME",
-            "1,3-dioxolane": "DOL",
-            "Aluminium Oxide": "Al2O3",
-            "Lithium Perchlorate": "LiClO4"
-        }
+
     def map_names(self, name_dict:dict=None, inplace=True):
         """ Rename the main dataframe in the dataset according to `name_dict`.
         Parameters
@@ -91,13 +86,21 @@ class LiquidMasterTableDataSet(AutomatDataSet):
         Returns
             dataframe: The renamed dataframe. 
         """
+
+        _name_dict = {
+            "1,2-dimethoxyethane": "DME",
+            "1,3-dioxolane": "DOL",
+            "Aluminium Oxide": "Al2O3",
+            "Lithium Perchlorate": "LiClO4"
+        }
+
         df_copy = self._df.copy()
-        if name_dict is None:
-            name_dict = self._name_dict
-        df_copy.rename(columns=name_dict, inplace=True)
+        if name_dict is not None:
+            _name_dict = name_dict
+        df_copy.rename(columns=_name_dict, inplace=True)
         if inplace:
             # Update chemical names
-            for old, new in self._name_dict.items():
+            for old, new in _name_dict.items():
                 if old in self.chemical_names:
                     self.chemical_names.remove(old)
                     self.chemical_names.append(new)
@@ -124,19 +127,19 @@ class LiquidMasterTableDataSet(AutomatDataSet):
         df_copy = self._df.copy()
         df_copy.fillna(0, inplace=True)
         return df_copy.loc[:, self.chemical_names]
-
-    @property
-    def lce(self):
-        df_copy = self._df.copy()
-        return df_copy.loc[:, self.lce_name]
     
     @property
     def electrolyte_ids(self):
         df_copy = self._df.copy()
         return df_copy.loc[:, "Electrolyte ID"]
     
-    def pull_data(self, subset="all"):
-        _df = pd.concat([self.chemicals, self.lce], axis=1)
+    
+    def target(self, target="LCE"):
+        df_copy = self._df.copy()
+        return df_copy.loc[:, target]
+    
+    def pull_data(self, subset="all", target="LCE"):
+        _df = pd.concat([self.chemicals, self.target(target)], axis=1)
         if subset == "lab":
             _df = _df.loc[~self._df["lab_batch"].isna()]
         elif subset == "ml":
@@ -147,6 +150,37 @@ class LiquidMasterTableDataSet(AutomatDataSet):
         X = torch.from_numpy(_df.iloc[:, :-1].to_numpy(dtype=float))
         y = torch.from_numpy(_df.iloc[:, [-1]].to_numpy(dtype=float))
         return X, y
+
+    def reduced_space(self, electrolyte_ids, target="LCE"):
+        """Given a list of electrolyte ID's, return only their non-zero components, with LCE.
+        """
+        _df = self.dataframe
+        indices = _df.index[_df["Electrolyte ID"].isin(electrolyte_ids)]
+        chemicals = self.chemicals.loc[indices]
+        absent_chemicals = chemicals.columns[(chemicals == 0).all(axis=0)]
+        df_reduced = pd.concat(
+            [
+                self.electrolyte_ids.loc[indices],
+                # Only chemicals in presence.
+                self.chemicals.loc[indices, ~chemicals.columns.isin(absent_chemicals)],
+                self.target(target)[indices]
+            ], axis=1
+        )
+    
+        return df_reduced
+
+    def parallel_plot(self, electrolyte_ids, target="LCE", title=None):
+        """Generate a parallel plot of non-zero components and LCE given a list of electrolyte ID's.
+        """
+        _df = self.reduced_space(electrolyte_ids, target=target)
+        fig, ax = plt.subplots(figsize=(_df.shape[1]-1, 4))
+        pd.plotting.parallel_coordinates(_df, class_column="Electrolyte ID", ax=ax)
+        ax.set_title(title)
+        ax.set_ylim([0,1])
+        ax.legend(loc="best")
+        plt.close()
+        return fig
+
 
 if __name__ == "__main__":
     db = Database(db="FMT")
